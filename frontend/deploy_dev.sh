65941edb675feb135dbed9782bafcdfcf0fe7b6e
wanglihx <wangli1@stu.scu.edu.cn>
2026-01-26 16:54:54 +0800

    feat: add AddBiomechanics full module

--- frontend/deploy_dev.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-amplify env checkout dev
-yarn build
-aws s3 cp build s3://dev.addbiomechanics.org --recursive
-aws cloudfront create-invalidation \
-    --distribution-id E3E4XU2LCU9D2 \
-    --paths "/*"
